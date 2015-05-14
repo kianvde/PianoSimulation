540c30b15732fe41caee8699ac781f2935f504c2
kianvde <Kianvde@gmail.com>
2015-05-14 12:02:31 +0200

    Fixed to loop over only the necessary files

--- renamer.py
+++ renamer.py
@@ -1,11 +1,13 @@
 __author__ = 'kian'
 import os
+import time
 # This file is needed to loop over the different keys and produce their .wav file
 # NB This uses the bash command 'sed' !
 letters = ['A','Ab','Ad','B','C','Cd','D','Dd','E','F','Fd','G']
 nums = ['3','4']
 moduleNext = None
 par = "Parameters.parameters"
+names = ['main', 'plot_and_save','update']
 for j, num in enumerate(nums):
     for i, letter in enumerate(letters[:-1]):
         bashPythoncommand = "python main.py"
@@ -21,7 +23,12 @@ for j, num in enumerate(nums):
             modulePrev = par+letter+num
             moduleNext = par+letters[i+1]+num
         print modulePrev, moduleNext
-        bashCommand_module = "sed -i -e 's/" + modulePrev +"/"+ moduleNext+"/g' "+ "./*.py"
-        bashCommand_filename = "sed -i -e 's/" + "piano"+tonePrev +"/"+ "piano"+toneNext+"/g' "+ "./*.py"
+        for name in names:
+            bashCommand_module = "sed -i -e 's/" + modulePrev +"/"+ moduleNext+"/g' "+ "./"+name+".py"
+            time.sleep(0.2)
+            bashCommand_filename = "sed -i -e 's/" + "piano"+tonePrev +"/"+ "piano"+toneNext+"/g' "+ "./"+name+".py"
+            time.sleep(0.2)
         os.system(bashCommand_module)
         os.system(bashCommand_filename)
+
+print moduleNext,modulePrev
